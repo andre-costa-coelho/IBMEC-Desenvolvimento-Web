b187af4c780ea1ea9719029b4ca5f4fdfb30ad47
André Costa <andreccoelho2004@gmail.com>
2024-05-06 11:10:29 -0300

    Criando CARDS

--- Exercícios/08/arrays-automatico/script.js
+++ Exercícios/08/arrays-automatico/script.js
@@ -21,3 +21,53 @@ function nomeCompletoForOf(array){
         console.log(`O nome completo de ${element.nome} é ${element.nome_completo}`);
     }
 }
+
+const atleta = dados[10];
+
+const conteudo = document.getElementById('conteudo');
+
+conteudo.innerHTML = `
+    <div class = 'card'>
+        <div class = 'imagem'>
+        <img src = "${atleta.imagem}" alt = "${atleta.nome}">
+        </div>
+        <div class = 'posicao'>
+        <p class='posicao'>${atleta.posicao}</p>
+        </div>
+        <div class = 'nome'>
+        <p class='nome'>${atleta.nome}</p>
+        </div>
+        <div class = 'descricao'>
+        <p class='descricao'>${atleta.descricao}</p>
+        </div>
+        <div class = 'nascimento'>
+        <p class='nascimento'>${atleta.nascimento}</p>
+        </div>
+    </div>
+ `;
+
+ function criarCards(array){
+    let cards = '';
+    array.forEach((element) => {
+        cards += `
+        <div class = 'card'>
+            <div class = 'imagem'>
+            <img src = "${element.imagem}" alt = "${element.nome}">
+            </div>
+            <div class = 'posicao'>
+            <p class='posicao'>${element.posicao}</p>
+            </div>
+            <div class = 'nome'>
+            <p class='nome'>${element.nome}</p>
+            </div>
+            <div class = 'descricao'>
+            <p class='descricao'>${element.descricao}</p>
+            </div>
+            <div class = 'nascimento'>
+            <p class='nascimento'>${element.nascimento}</p>
+            </div>
+        </div>
+        `;
+    });
+    conteudo.innerHTML = cards;
+}
